0dd41e0972b22575f40bf363d4dffc56cbaa67a1
Amelieumutoni <a.umutoni@alustudent.com>
2023-11-28 09:07:24 -0800

    good

--- KidAcademy/main.py
+++ KidAcademy/main.py
@@ -62,7 +62,6 @@ def login():
     else:
         print("Incorrect password.")
 
-
 def create_content(current_user):
     print(f"User '{current_user}' - Create content:")
     content_text = input("Enter your content: ")
@@ -72,11 +71,46 @@ def create_content(current_user):
     print("Content created successfully.")
     [200~def view_content():
                 print("Viewing content:")
-                    for username, data in users.items():
+                for username, data in users.items():
                             for content in data["content"]:
-                                        print(f"{username}: {content}")
+                            print(f"{username}: {content}")
+                            def view_content():
+    print("Viewing content:")
+    for username, data in users.items():
+        for content in data["content"]:
+            print(f"{username}: {content}")
 
 
-                                        def main():
-                                            current_user = None
+def main():
+    current_user = None
 
+    while True:
+        print("\nOptions:")
+        print("1. Register")
+        print("2. Login")
+        print("3. Create Content")
+        print("4. View Content")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == "1":
+            register()
+        elif choice == "2":
+            current_user = login()
+        elif choice == "3":
+            if current_user:
+                create_content(current_user)
+            else:
+                print("Please log in first.")
+        elif choice == "4":
+            view_content()
+        elif choice == "5":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.")
+
+
+if _name_ == "_main_":
+    main()
