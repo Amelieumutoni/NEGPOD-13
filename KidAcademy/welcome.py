137cd17c69a3acff5279e1e27f02f0f658489625
buwituze <b.rutagengw@alustudent.com>
2023-11-28 10:31:25 -0800

    Added welcome.py

---
+++ KidAcademy/welcome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def introduction():
+    print("Welcome to KidAcademy!")
+    print("This app is designed to...")
+    print("This platform is provided to help the learning process of young students. Let's learning culture with a sauce of fun!")
+    print("Tour the App")
+    print("We hope you have a great experience!")
+    print("----------------------------")
